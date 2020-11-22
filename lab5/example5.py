ce0bc90b5c5cc4f24290f2833dd0a8880cb17488
Hakancoban <Raltek9@hotmail.com>
2020-11-22 13:22:06 +0000

    Added fibonacci sequance

--- lab5/example5.py
+++ lab5/example5.py
@@ -37,3 +37,13 @@ print((even/numbers)*100)
 
 #New example
 
+a = int(input("Range of fibonacci: "))
+n1 = 0
+n2 = 1
+
+for i in range(1,a+1):
+  n3= n2+n1
+  n1 = n2
+  n2 = n3
+  print(n3)
+
--- labx/examplex.py
+++ labx/examplex.py
@@ -1,4 +1,12 @@
+a = int(input("Range of fibonacci: "))
+n1 = 0
+n2 = 1
 
+for i in range(1,a+1):
+  n3= n2+n1
+  n1 = n2
+  n2 = n3
+  print(n3)
 
 
 
